ac8090139a83852fc9b1bbf2382eb69c7aceb804
Dani Garrido <83754027+DaniGarridoNunez@users.noreply.github.com>
2024-05-15 16:31:06 +0200

    Cambios de estructura

---
+++ public/build/js/app.js
@@ -0,0 +1,36 @@
+document.addEventListener('DOMContentLoaded', () => {
+    eventListeners();
+});
+
+function eventListeners() {
+    const menu = document.querySelector('.menu-img');
+    const main = document.querySelector('main');
+
+    // Open menu on mouseover and click
+    menu.addEventListener('mouseover', openMenu);
+    menu.addEventListener('click', openMenu);
+
+    // Close menu on clicking outside
+    document.addEventListener('click', closeMenu);
+    document.addEventListener('touchstart', closeMenu);
+}
+
+function openMenu(event) {
+    event.stopPropagation();
+    const nav = document.querySelector('.sub-menu');
+    nav.classList.add('visible');
+    
+    // Add event listener for mouse leave
+    nav.addEventListener('mouseleave', () => {
+        nav.classList.remove('visible');
+    });
+}
+
+function closeMenu(event) {
+    const nav = document.querySelector('.sub-menu');
+    const menu = document.querySelector('.menu-img');
+
+    if (!nav.contains(event.target) && !menu.contains(event.target)) {
+        nav.classList.remove('visible');
+    }
+}
